b6d314c5c1f0aeee614aaf115dff9c25faa383db
Nick Xenos <nxenos@spektre.ai>
2021-08-06 03:49:34 +0800

    Add the exception handling, the db lookup, domain regex

--- controllers/spoof_check_api.py
+++ controllers/spoof_check_api.py
@@ -1,16 +1,13 @@
+import os
 from flask_restful import Resource, reqparse, request, inputs
-import socket
-import pymongo
 from enum import Enum
-from datetime import datetime, timedelta
-import validators
-from helpers import auth_check, spoofcheck
-from helpers.mongo_connection import db
+from helpers import auth_check, spoofcheck, common_strings, logging_setup, queue_to_db, utils
 
 portscan_args = reqparse.RequestParser()
 
-portscan_args.add_argument('value', help='Domain or IP is required to scan', required=True)
-portscan_args.add_argument('force', type=inputs.boolean, default=False)
+portscan_args.add_argument(common_strings.strings['key_value'], help=common_strings.strings['domain_required'], required=True)
+portscan_args.add_argument(common_strings.strings['input_force'], type=inputs.boolean, default=False)
+logger = logging_setup.initialize(common_strings.strings['spoofcheck'], 'logs/spoofcheck_api.log')
 
 class Risk(Enum):
     FAIL = "FAIL"
@@ -20,50 +17,65 @@ class SpoofCheck(Resource):
 
     @staticmethod
     def post():
-        auth = request.headers.get('Authorization')
+        args = portscan_args.parse_args()
+
+        value = args['value']
+
+        logger.debug(f"spoofcheck request received for {value}")
+
+        auth = request.headers.get(common_strings.strings['auth'])
 
         authentication = auth_check.auth_check(auth)
 
         if authentication['status'] == 401:
+            logger.debug(f"Unauthenticated WhoIs request received for {value}")
             return authentication, 401
 
-        args = portscan_args.parse_args()
+        if not utils.validate_domain(value):  # if regex doesn't match throw a 400
+            logger.debug(f"Domain that doesn't match regex request received - {value}")
+            return {
+                       common_strings.strings['message']: f"{value}" + common_strings.strings['invalid_domain']
+                   }, 400
+        
+        # if domain doesn't resolve into an IP, throw a 400 as domain doesn't exist in the internet
+        try:
+            ip = utils.resolve_domain_ip(value)
+        except Exception as e:
+            logger.debug(f"Domain that doesn't resolve to an IP was requested - {value, e}")
+            return {
+                       common_strings.strings['message']: f"{value}" + common_strings.strings['unresolved_domain_ip']
+                   }, 400
 
-        list_scans = {}
-        val = args['value']
-        db.spoofCheck.create_index('value')
-        # see if we have an existing scan for given value and pull the latest
-        search = db.spoofCheck.find_one({'value': val}, sort=[('_id', pymongo.DESCENDING)])
-
-        # force comes in as false by default
-        if args['force']:
+        if args[common_strings.strings['input_force']]:
             force = True
-        elif search is not None:
-            force = search['timeStamp'] + timedelta(days=2) < datetime.utcnow()
-
-        if search is None or force is True:
-
-            if validators.domain(val) or validators.ip_address.ipv4(val):
-                ip = socket.gethostbyname(val)
-                # add the actual value in(URL)
-                item = db.spoofCheck.insert_one(
-                    {'ip': ip, 'value': val,
-                     "timeStamp": datetime.utcnow()}).inserted_id
-
-                list_scans['ip'] = ip
-                list_scans['value'] = val
-
-                out = spoofcheck.main_check(val)
-
-                list_scans.update(out)
-                list_scans['risk'] = Risk.FAIL.name if list_scans['message'].count("possible")>0 else Risk.PASS.name
-            else:
-                return {
-                           'message': f'{val} is not a valid IP or Domain, please try again'
-                       }, 400
         else:
-            del search['_id']
-            del search['timeStamp']
-            return search
+            force = False  
 
-        return list_scans
+        # based on force - either gives data back from database or gets a True back to continue with a fresh scan
+        check = utils.check_force(value, force, collection=common_strings.strings['spoofcheck'],
+                                  timeframe=int(os.environ.get('DATABASE_LOOK_BACK_TIME')))    
+        
+        # if a scan is already requested/in-process, we send a 202 indicating that we are working on it
+        if check == common_strings.strings['status_running'] or check == common_strings.strings['status_queued']:
+            return {'status': check}, 202
+        
+        # if database has an entry with results and force is false, send it
+        elif type(check) == dict and check['status'] == common_strings.strings['status_finished']:
+            logger.debug(f"spoofcheck scan response sent for {value} from database lookup")
+            return check['output'], 200
+
+        else:
+            # mark in db that the scan is queued
+            utils.mark_db_request(value, status=common_strings.strings['status_queued'],
+                                  collection=common_strings.strings['spoofcheck'])
+            output = {common_strings.strings['key_value']: value, common_strings.strings['key_ip']: ip}
+        
+        try:
+            spoofcheck_data = spoofcheck.main_check(value)
+            output.update(spoofcheck_data)
+            output['risk'] = Risk.PASS.name if output['message'].count("not possible")>0 else Risk.FAIL.name
+        except Exception as e:
+            logger.critical(common_strings.strings['error'], exc_info=e)
+            return 'spoofcheck scan is currently unavailable', 503
+        queue_to_db.spoofcheck_response_db_addition(value, output)
+        return output, 200
---
+++ helpers/common_strings.py
@@ -0,0 +1,17 @@
+strings = {
+    'invalid_domain': ' is not a valid domain, please try again',
+    'unresolved_domain_ip': ' is not available or cannot be reached right now, please check and try again later',
+    'domain_required': 'Domain is required to scan',
+    'auth': 'Authorization',
+    'status_queued': 'queued',
+    'status_running': 'running',
+    'status_finished': 'finished',
+    'database_issue': 'Database connection issue',
+    'spoofcheck': 'spoofCheck',
+    'key_value': 'value',
+    'key_ip': 'ip',
+    'mongo_value': 'value',
+    'input_force': 'force',
+    'error': 'error',
+    'message': 'message',
+}
